67e2ed7f7cb4484a67ce1ae48ca8a2833f858e31
Jason <gatesjasond@gmail.com>
2024-06-12 21:26:45 -0500

    FEAT: Added api to complete todo

--- main.go
+++ main.go
@@ -19,10 +19,12 @@ func main(){
 
 	todos := []Todo{}
 
+	// Get todos
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
 	})
 
+	// Create Todo
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{}
 
@@ -38,6 +40,20 @@ func main(){
 		todos = append(todos, *todo)
 
 		return c.Status(201).JSON(todo)
+	})
+
+	// Complete todo
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error":"Todo not found"})
 	})
 	log.Fatal(app.Listen(":4000"))
 }
