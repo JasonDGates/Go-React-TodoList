ef2dd4bc006a4b85f66fd0c3c22c9c7b1371d2b3
Jason <gatesjasond@gmail.com>
2024-06-12 22:08:45 -0500

    FEAT: Added mongodb connection

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct {
-	ID int `json:"id"`
+	ID int `json:"_id" bson:"_id`
 	Completed bool `json:"completed"`
 	Body string `json:"body"`
 }
+
+var collection *mongo.Collection
 
 func main(){
 	fmt.Println("Hello World")
@@ -25,7 +30,21 @@ func main(){
 	}
 
 	PORT := os.Getenv("PORT")
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI((MONGODB_URI))
+	client,err := mongo.Connect(context.Background(), clientOptions)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connection to MongoDB")
 	todos := []Todo{}
 
 	// Get todos
